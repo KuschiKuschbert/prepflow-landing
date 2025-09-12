bdb0a0631c2fec812622e0b54a8f2df09625342c
KuschiKuschbert <derkusch@gmail.com>
2025-09-12 23:47:36 +1000

    fix: remove .next build files from git and update gitignore

--- .next/static/chunks/app_layout_tsx_0a548d63._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__28bc9c2a._.css",
-  "static/chunks/_6222251c._.js",
-  "static/chunks/node_modules_627ce1a9._.js"
-],
-    source: "dynamic"
-});
