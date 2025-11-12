813a104d1cb0c3912ca9fe923d16128cf306f0ad
KuschiKuschbert <derkusch@gmail.com>
2025-11-12 21:30:44 +1000

    fix: add loadExistingRecipeIngredients to CogsClient destructuring

--- app/webapp/cogs/components/CogsClient.tsx
+++ app/webapp/cogs/components/CogsClient.tsx
@@ -45,6 +45,7 @@ export default function CogsClient() {
     addCalculation,
     loadCalculations,
     checkRecipeExists,
+    loadExistingRecipeIngredients,
     setError,
   } = useCOGSCalculations();
 
